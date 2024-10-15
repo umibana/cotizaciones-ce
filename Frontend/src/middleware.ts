7a5b73a0c0ea82d763ab7596a5f686b39d57239f
umibana <hans.villarroel@usach.cl>
2024-10-15 00:33:14 -0300

    middleware working

--- Frontend/src/middleware.ts
+++ Frontend/src/middleware.ts
@@ -1,13 +1,14 @@
 import { authMiddleware } from "better-auth/next-js";
 import { NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
 
 export default authMiddleware({
     customRedirect: async (session, request) => {
         const baseURL = request.nextUrl.origin;
-        if (request.nextUrl.pathname === "/login" && session) {
+        if (request.nextUrl.pathname === "/sign-in" && session) {
             return NextResponse.redirect(new URL("/dashboard", baseURL));
         }
-        if (request.nextUrl.pathname === "/dashboard" && !session) {
+        if (request.nextUrl.pathname === "/dashboard" || request.nextUrl.pathname === "/" && !session) {
             return NextResponse.redirect(new URL("/login", baseURL));
         }
         return NextResponse.next();
@@ -15,5 +16,5 @@ export default authMiddleware({
 });
 
 export const config = {
-    matcher: ["/dashboard", "/login"],
+    matcher: ["/", "/dashboard", "/login"],
 };
