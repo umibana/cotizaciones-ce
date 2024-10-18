b6fb1397ab3a23e06bb6b3923f640c8d55a4906f
umibana <hans.villarroel@usach.cl>
2024-10-18 00:43:50 -0300

    SignOut in menubar

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { client, signOut } from "@/lib/auth-client";
 import { ArrowLeft, LogOut } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 export default function Navbar() {
 	const router = useRouter();
+
+	const logOut = async () => {
+		try {
+			await client.signOut();
+		} catch (error) {
+			if (error instanceof Error) {
+				console.log(error.message);
+			} else {
+				console.log("An unknown error occurred");
+			}
+		} finally {
+			router.push("/login");
+		}
+	};
 
 	return (
 		<div className="w-full h-14 bg-background border-b flex items-center justify-between px-4">
@@ -15,9 +30,9 @@ export default function Navbar() {
 					aria-label="Go back">
 					<ArrowLeft className="h-4 w-4" />
 				</Button>
-				<div className="font-semibold text-lg ml-4">Logo</div>
+				<div className="font-semibold text-lg ml-4">Casa Experto</div>
 			</div>
-			<Button variant="ghost" size="sm">
+			<Button variant="ghost" size="sm" onClick={async () => await logOut()}>
 				<LogOut className="h-4 w-4 mr-2" />
 				Sign out
 			</Button>
