c07c88ad8f1bc101f2900c98714c0dc7b94356f0
umibana <hans.villarroel@usach.cl>
2024-11-25 03:41:50 -0300

    Feat: [HU-4 en progreso] Cambio de css, componente de Proyecto por defecto en home

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -2,59 +2,177 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useRoles } from "@/hooks/useRoles";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { FaTrash } from "react-icons/fa";
+import { useAuthenticatedQuery } from "@/hooks/useAuth";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// Interfaces de typescript
+interface ProjectListProps {
+	projects: any[]; // Reemplazar con la interfaz de proyecto cuando esté lista
+	onAssign?: (id: string) => void;
+	onReview?: (id: string) => void;
+	role: "admin" | "maestro" | "supervisor";
+}
+
+const UnassignedProjectList = ({
+	projects,
+	onAssign,
+	role,
+}: ProjectListProps) => {
+	return (
+		<Card className="flex-1">
+			<CardHeader className="flex flex-row items-center justify-between">
+				<CardTitle>Proyectos sin asignar</CardTitle>
+				<Link href="/nuevo-proyecto">
+					<Button size="default" aria-label="Crear nueva cotización">
+						Crear Nuevo Proyecto
+					</Button>
+				</Link>
+			</CardHeader>
+			<CardContent>
+				<ScrollArea className="h-[200px] w-full rounded-md border p-4">
+					{projects.map((project: any) => (
+						<div
+							key={project.idProyecto}
+							className="mb-2 flex items-center justify-between rounded-lg bg-muted p-2">
+							<span className="text-lg font-bold">{project.nombre}</span>
+							<div className="flex gap-2">
+								<Link
+									href={{
+										pathname: "/asignacion",
+										query: { id: project.idProyecto },
+									}}>
+									<Button size="sm">Asignar</Button>
+								</Link>
+								{role === "admin" && (
+									<Button size="sm" onClick={() => onAssign?.(project.id)}>
+										<FaTrash />
+									</Button>
+								)}
+							</div>
+						</div>
+					))}
+				</ScrollArea>
+			</CardContent>
+		</Card>
+	);
+};
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const AssignedProjectList = ({
+	projects,
+	onReview,
+}: // role,
+ProjectListProps) => (
+	<Card className="flex-1">
+		<CardHeader>
+			<CardTitle>Proyectos asignados</CardTitle>
+		</CardHeader>
+		<CardContent>
+			<ScrollArea className="h-[200px] w-full rounded-md border p-4">
+				{projects.map((project: any) => (
+					<div
+						key={project.idProyecto}
+						className="mb-2 flex items-center justify-between bg-muted  p-2">
+						<span className="text-lg font-bold">{project.nombre}</span>
+						<div className="flex items-center gap-2">
+							<span className="font-medium">${project.price}</span>
+							<Button
+								size="sm"
+								variant="outline"
+								onClick={() => onReview?.(project.id)}>
+								Revisar
+							</Button>
+						</div>
+					</div>
+				))}
+			</ScrollArea>
+		</CardContent>
+	</Card>
+);
+
+interface ProyectosProps {
+	role: "admin" | "maestro" | "supervisor";
+}
+
+function Proyectos({ role }: ProyectosProps) {
+	// Endpoint de la API según el rol, deberia cambiarse después
+	const endpoint = role === "maestro" ? "/proyectos/all" : "/proyectos/all";
+
+	// Obtener los datos de la API usando hook de useAuth
+	const { data, isLoading, error } = useAuthenticatedQuery<any[]>(
+		["projects", role],
+		`${process.env.NEXT_PUBLIC_BACKEND_URL}${endpoint}`
+	);
+
+	if (isLoading) return <div className="text-center p-4">Loading...</div>;
+
+	if (error)
+		return (
+			<div className="text-center p-4 text-red-500">
+				An error occurred: {(error as Error).message}
+			</div>
+		);
+
+	// Aca esta temporal, deberia manejarse diferente si es maestro
+	const projects = {
+		unassigned:
+			role === "maestro"
+				? []
+				: data?.filter((project) => project.estado === "Sin asignar") ?? [],
+		assigned:
+			role === "maestro"
+				? data ?? []
+				: data?.filter((project) => project.estado !== "Sin asignar") ?? [],
+	};
+
+	return (
+		<div className="container mx-auto p-4 space-y-4">
+			{(role === "admin" || role === "supervisor") && (
+				<UnassignedProjectList
+					projects={projects.unassigned}
+					// onAssign={handleAssign}
+					role={role}
+				/>
+			)}
+			<AssignedProjectList
+				projects={projects.assigned}
+				// onReview={handleReview}
+				role={role}
+			/>
+		</div>
+	);
+}
 
 function App() {
-	const { isAdmin, isMaestro, isAuthenticated } = useRoles();
+	const { isAdmin, isMaestro, isSupervisor, isAuthenticated } = useRoles();
+
+	// Si no está autenticado, no se muestra nada
+	if (!isAuthenticated) {
+		return <p>No tienes permisos para acceder a esta vista</p>;
+	}
+
+	// Determinar el rol del usuario
+	const role = isAdmin
+		? "admin"
+		: isMaestro
+		? "maestro"
+		: isSupervisor
+		? "supervisor"
+		: null;
+
+	if (!role) {
+		return <p>No tienes permisos para acceder a esta vista</p>;
+	}
 
 	return (
 		<div className="flex flex-auto align-middle flex-col justify-center items-center container mx-auto p-4 space-y-4">
 			<h1 className="text-xl font-extrabold">Prueba de Vistas Casa Experto</h1>
-
-			{isAdmin && <AdminView />}
-
-			{isMaestro && <MaestroView />}
-
-			{isAuthenticated && !isAdmin && !isMaestro && (
-				<p>No tienes permisos para acceder a esta vista</p>
-			)}
-		</div>
-	);
-}
-
-// Separate components for better organization
-function AdminView() {
-	return (
-		<div className="text-center flex flex-col gap-2">
-			<h1 className="text-xl font-bold">Vista Jefe</h1>
-			<Button asChild>
-				<Link href="/asignacion">Asignación</Link>
-			</Button>
-			<Button asChild>
-				<Link href="/proyectos">Proyectos</Link>
-			</Button>
-			<Button asChild>
-				<Link href="/materiales">Materiales</Link>
-			</Button>
-			<Button asChild>
-				<Link href="/nuevo-proyecto">Nuevo Proyecto</Link>
-			</Button>
-			<Button asChild>
-				<Link href="/admin">Administración</Link>
-			</Button>
-		</div>
-	);
-}
-
-function MaestroView() {
-	return (
-		<div className="text-center flex flex-col gap-2">
-			<h1 className="text-xl font-bold">Vista Maestro</h1>
-			<Button asChild>
-				<Link href="/cotizacion">Cotización</Link>
-			</Button>
-			<Button asChild>
-				<Link href="/proyectos-maestro">Proyectos</Link>
-			</Button>
+			{/* Renderizar el componente de proyectos según su rol*/}
+			<Proyectos role={role} />
 		</div>
 	);
 }
--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 	const router = useRouter();
 
 	return (
-		<nav className="bg-white shadow">
+		<nav className="bg-muted-foreground text-muted shadow-lg">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex justify-between h-16">
 					{/* Left side - Back Arrow and Logo */}
