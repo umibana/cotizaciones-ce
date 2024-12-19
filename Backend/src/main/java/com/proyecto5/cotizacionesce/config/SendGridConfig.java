5589315e883ae31122574e6690f6860f01e8498d
umibana <hans.villarroel@usach.cl>
2024-12-18 22:41:58 -0300

    Fix: Cambio de nombre de variable de api key sendgrid

--- Backend/src/main/java/com/proyecto5/cotizacionesce/config/SendGridConfig.java
+++ Backend/src/main/java/com/proyecto5/cotizacionesce/config/SendGridConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SendGridConfig {
 
-    @Value("${SendGrid.api.key}")
+    @Value("${sendgrid.api.key}")
     private String sendGridApiKey;
 
     @Bean
